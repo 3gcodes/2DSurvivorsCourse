142abcf6e35c3e655e332e2f66bcc923df303ebd
Gregg Bolinger <gbolinger@worktorch.io>
2023-03-17 16:54:26 -0500

    more stuffs

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -2,6 +2,7 @@ extends Node
 class_name HealthComponent
 
 signal die
+signal heath_changed
 
 @export var max_health: float = 10
 var current_health
@@ -14,7 +15,16 @@ func _ready():
 func damage(damage_amt: float):
 	# don't drop below zero
 	current_health = max(current_health - damage_amt, 0)
+	heath_changed.emit()
 	Callable(check_death).call_deferred()
+	
+
+func get_health_percent():
+	if max_health <= 0:
+		return 0
+		
+	return min(current_health / max_health, 1)
+	
 	
 
 func check_death():
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,6 +3,19 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
+
+var number_coliding_bodies = 0
+
+func _ready():
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timeout)
+	health_component.heath_changed.connect(on_heath_changed)
+	update_heath_display()
+
 
 func _process(delta):
 	var movement_vector = get_movement_vector()
@@ -15,3 +28,33 @@ func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
+	
+	
+func check_deal_damage():
+	if number_coliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+		
+	health_component.damage(1)
+	damage_interval_timer.start()
+	print(health_component.current_health)
+	
+
+func update_heath_display():
+	health_bar.value = health_component.get_health_percent()
+
+
+func on_body_entered(other_body: Node2D):
+	number_coliding_bodies += 1
+	check_deal_damage()
+	
+
+func on_body_exited(other_body: Node2D):
+	number_coliding_bodies -= 1
+	
+	
+func on_damage_interval_timeout():
+	check_deal_damage()
+	
+	
+func on_heath_changed():
+	update_heath_display()
